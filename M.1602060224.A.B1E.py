927e651a2cc4a081b62ec54130d943bc6d32b6d4
m9810223 <39259397+m9810223@users.noreply.github.com>
2020-10-10 00:12:32 +0800

    Create M.1602060224.A.B1E.py

---
+++ M.1602060224.A.B1E.py
@@ -0,0 +1,16 @@
+ helper(arr, num):
+    subs = {()}
+    for a in arr:
+        subs |= { s + (a, ) for s in subs }
+    re = []
+    for _ in subs:
+      if sum(_) == num:
+        re.append(_)
+    return re
+
+t = [821,225,1821,38,1888,843,1517,143,43,16,1120,1714]
+num = 2018
+
+
+rt = helper(t, num)
+print(rt)
