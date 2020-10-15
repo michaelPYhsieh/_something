e47424d4d9ceffeb19fb5e8b69ebc232cbb201b2
m9810223 <39259397+m9810223@users.noreply.github.com>
2020-10-15 23:47:56 +0800

    Create 119. Pascal's Triangle II.py

---
+++ 119. Pascal's Triangle II.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        e = 1
+        re = [e]
+        for i in range(rowIndex):
+            e = int(e*(rowIndex-i)/(i+1))
+            re.append(e)
+        return re
